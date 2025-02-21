28727add359ad031b83e40700479c322ded07870
Lobe <lobe350@gmail.com>
2025-02-21 17:10:43 +0200

    feat: added pricing section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,15 @@
+import dynamic from 'next/dynamic';
 import CallToAction from './_components/call-to-action';
 import DashboardSnippet from './_components/dashboard-snippet';
+
+const PricingSection = dynamic(
+  () =>
+    import('./_components/pricing').then(
+      (component) => component.PricingSection
+    ),
+  { ssr: true }
+);
+
 export default function Home() {
   return (
     <main className='md:px-10 py-20 flex flex-col gap-36'>
@@ -7,6 +17,7 @@ export default function Home() {
         <CallToAction />
         <DashboardSnippet />
       </div>
+      <PricingSection />
     </main>
   );
 }
