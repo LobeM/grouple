04e5f7364eac78e30b1f90b442544186b6c1678f
Lobe <lo.musonda@pacra.org.zm>
2025-02-21 15:46:05 +0200

    feat: added call to action

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,11 @@
+import CallToAction from './_components/call-to-action';
+
 export default function Home() {
-  return <div className=''></div>;
+  return (
+    <main className='md:px-10 py-20 flex flex-col gap-36'>
+      <div>
+        <CallToAction />
+      </div>
+    </main>
+  );
 }
