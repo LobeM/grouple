7dc0f2878f4a8ac1462532b8812a4d7d5b060d4a
Lobe <lobe350@gmail.com>
2025-03-10 16:24:11 +0200

    feat: add group creation page with affiliate referral support

---
+++ src/components/forms/create-group/index.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  userId: string;
+  affiliate: boolean;
+  stripeId?: string;
+};
+
+const CreateGroup = ({ userId, affiliate, stripeId }: Props) => {
+  return <div>CreateGroup</div>;
+};
+
+export default CreateGroup;
